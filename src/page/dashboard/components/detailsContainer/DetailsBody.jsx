eaad9b14de1b13b951b8e0dd5d0f55ac8d708b1b
Sanigamk <sanigamk@gmail.com>
2024-10-05 16:10:59 +0530

    created dashbordgrid

--- src/page/dashboard/components/detailsContainer/DetailsBody.jsx
+++ src/page/dashboard/components/detailsContainer/DetailsBody.jsx
@@ -1,5 +1,15 @@
+import { DetailsItems } from "./DetailsItems";
+
 function DetailsBody() {
-  return <div className="detailsContainer__body"></div>;
+  return <div className="detailsContainer__body detailsContainer2 ">
+    <p>Your top 4 items</p>
+    <div className="detailsContainer__griid">
+    <DetailsItems/>
+    <DetailsItems/>
+    <DetailsItems/>
+    <DetailsItems/>
+    </div>
+  </div>;
 }
 
 export default DetailsBody;
---
+++ src/page/dashboard/components/detailsContainer/DetailsItems.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const DetailsItems = () => {
+    return (
+
+        <div className="detailsContainer__griid_items">
+
+            <div className='product_item'></div>
+
+            <div className='detailsContainer2__item_details'>
+                <div className='product_item_detail'>
+                    <p>item title here</p>
+                    <p>100USD</p>
+                </div>
+                <div>
+                    <p>12</p>
+                    <p>7</p>
+                </div>
+            </div>
+
+        </div>
+
+
+
+    )
+}
