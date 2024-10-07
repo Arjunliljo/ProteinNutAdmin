0f60a7d12851a6c7cfec7f600227163b1066e593
Sanigamk <sanigamk@gmail.com>
2024-10-07 13:11:39 +0530

    dashboradheader

---
+++ src/page/dashboard/components/detailsContainer/DetailHeaderContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const DetailHeaderContent = () => {
+  return (
+        <div className='header_item'>
+            <div>
+                 <h2>Logo</h2>
+            </div>
+            <div className='headers_itemlevel'>
+                <h1 style={{fontWeight:"bold",fontSize:"25px"}}>6784</h1>
+                <h4>Total Deliveries</h4>
+            </div>
+        </div> 
+    
+  )
+}
--- src/page/dashboard/components/detailsContainer/DetailsHeader.jsx
+++ src/page/dashboard/components/detailsContainer/DetailsHeader.jsx
@@ -1,5 +1,23 @@
+import { DetailHeaderContent } from "./DetailHeaderContent";
+
 function DetailsHeader() {
-  return <div className="detailsContainer__header"></div>;
+  return <div className="detailsContainer__header">
+    <div className="headerlogo">
+      <img src="./images/Logo.svg" className="imageheader" alt="" />
+    </div>
+    <div>
+      <div>
+        <h1 style={{fontWeight:"bold",fontSize:"20px"}}>September 2024</h1>
+      </div >
+      <div className="headerOne">
+        <DetailHeaderContent />
+        <DetailHeaderContent />
+        <DetailHeaderContent />
+      </div>
+
+    </div>
+
+  </div>;
 }
 
 export default DetailsHeader;
